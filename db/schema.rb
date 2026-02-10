4a58889a86c88cd5ed60ae6c914e8d1c3db9c96e
Benjamin Block <bjblock@gmail.com>
2026-02-10 14:03:42 -0600

    fixes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_02_04_201519) do
+ActiveRecord::Schema[8.1].define(version: 2022_02_04_201519) do
   create_table "transactions", force: :cascade do |t|
-    t.integer "from_user_id"
-    t.integer "to_user_id"
     t.integer "amount"
     t.datetime "created_at", null: false
+    t.integer "from_user_id"
+    t.integer "to_user_id"
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
-
 end
